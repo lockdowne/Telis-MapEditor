d9da2a45d007b7e0468e300ff949cdb85a53e131
lockdowne <lockdowne@gmail.com>
2014-04-09 19:48:44 -0700

    Added redo methods for draw/erase

--- MapEditor/Core/Commands/EditRemoveCommand.cs
+++ MapEditor/Core/Commands/EditRemoveCommand.cs
@@ -9,29 +9,52 @@ namespace MapEditor.Core.Commands
 {
     public class EditRemoveCommand : ICommand
     {
-        private Layer currentLayer;
-        private Layer previousLayer;
+        private Layer layer;
 
-        private List<TileBrush> tileBrushes;
+        private List<TileBrush> currentTileBrushes;
+        private List<TileBrush> previousTileBrushes;
 
         public EditRemoveCommand(Layer layer, List<TileBrush> brushes)
         {
-            currentLayer = layer;
+            this.layer = layer;
 
-            tileBrushes = brushes;
+            this.currentTileBrushes = new List<TileBrush>(brushes.AsEnumerable());
+
+            this.previousTileBrushes = new List<TileBrush>();
         }
 
         public void Execute()
         {
-            previousLayer = currentLayer;
-
-            tileBrushes.ForEach(brush =>
+            currentTileBrushes.ForEach(brush =>
             {
+                int[,] previousBrush = new int[brush.Brush.GetLength(0), brush.Brush.GetLength(1)];
+
+                // Get previous tiles
                 for (int y = 0; y < brush.Brush.GetLength(0); y++)
                 {
                     for (int x = 0; x < brush.Brush.GetLength(1); x++)
                     {
-                        currentLayer.Rows[(int)(y + brush.Position.Y)].Columns[(int)(x + brush.Position.X)].TileID = brush.Brush[y, x];
+                        previousBrush[y, x] = layer.Rows[(int)(y + brush.Position.Y)].Columns[(int)(x + brush.Position.X)].TileID;
+                    }
+                }
+
+                // Add previous tiles
+                previousTileBrushes.Add(new TileBrush()
+                {
+                    Brush = previousBrush,
+                    Position = brush.Position,
+                });
+
+            });
+
+            currentTileBrushes.ForEach(brush =>
+            {
+                // Draw new tiles
+                for (int y = 0; y < brush.Brush.GetLength(0); y++)
+                {
+                    for (int x = 0; x < brush.Brush.GetLength(1); x++)
+                    {
+                        layer.Rows[(int)(y + brush.Position.Y)].Columns[(int)(x + brush.Position.X)].TileID = brush.Brush[y, x];
                     }
                 }
             });
@@ -39,7 +62,16 @@ namespace MapEditor.Core.Commands
 
         public void UnExecute()
         {
-
+            previousTileBrushes.ForEach(brush =>
+            {
+                for (int y = 0; y < brush.Brush.GetLength(0); y++)
+                {
+                    for (int x = 0; x < brush.Brush.GetLength(1); x++)
+                    {
+                        layer.Rows[(int)(y + brush.Position.Y)].Columns[(int)(x + brush.Position.X)].TileID = brush.Brush[y, x];
+                    }
+                }
+            });
         }
     }
 }
